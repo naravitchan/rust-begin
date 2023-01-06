bdc8087bd51bc0de416f7430ba00ca335485c7d4
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-01-06 14:03:23 +0700

    [#6] - ownership & ref

--- sec6/ownership.rs
+++ sec6/ownership.rs
@@ -4,9 +4,17 @@ fn main() {
     // println!("{}", s); error[E0382]: borrow of moved value: `s`
     s = take(s);
     println!("{}", s);
+
+    let a = String::from("hello Ref");
+    take_ref(&a);
+    println!("{}", a);
 }
 
 fn take(s1: String) -> String {
     println!("{}", s1);
     s1 // return s1 to fix moved error
+}
+
+fn take_ref(s1: &String) {
+    println!("{}", s1);
 }
