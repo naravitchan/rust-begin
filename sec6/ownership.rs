99002db1523b7b74999a984748979380b664efb1
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-01-06 12:00:36 +0700

    [#6] - ownership & borrowing

--- sec6/ownership.rs
+++ sec6/ownership.rs
@@ -1,9 +1,12 @@
 fn main() {
-    let s = String::from("hello");
-    take(s);
+    let mut s = String::from("hello");
+    // take(s);
     // println!("{}", s); error[E0382]: borrow of moved value: `s`
+    s = take(s);
+    println!("{}", s);
 }
 
-fn take(s1: String) {
+fn take(s1: String) -> String {
     println!("{}", s1);
+    s1 // return s1 to fix moved error
 }
