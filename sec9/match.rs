d1b41e8243f8c181cb167d498cbf7b9dc3750090
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-01-12 14:32:18 +0700

    [#9] - match/option

--- sec9/match.rs
+++ sec9/match.rs
@@ -3,38 +3,28 @@ enum Coin{
 	Penny,
 	Nickel,
 	Dime,
-	Quarter,
+	Quarter(UsState),
+}
+#[allow(dead_code)]
+#[derive(Debug)]
+enum UsState{
+	Alaska,
+	Arizona,
+	//etc
 }
 fn value_in_cents(c:Coin)->u32{
 	match c {
-		Coin::Penny=>{
-			println!("Penny");
-			1
-		},
-		Coin::Nickel=>{
-		println!("Nickel");
-		5
-		
-		},
+		Coin::Penny=>1,
+		Coin::Nickel=>5,
 		Coin::Dime=>10,
-		Coin::Quarter=>25,
+		Coin::Quarter(state)=>{
+			println!("{:?}",state);
+			25
+		},
 	}
 
 }
 
 fn main(){
-	
-	println!("{}",value_in_cents(Coin::Nickel));
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
+	println!("{}",value_in_cents(Coin::Quarter(UsState::Alaska)));
 }
