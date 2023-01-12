1f5969198881b6e70143ffae2de05ff4b87ff811
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-01-12 10:41:26 +0700

    [#9] - enum

---
+++ sec9/enum.rs
@@ -0,0 +1,21 @@
+// #[allow(dead_code)]
+#[allow(non_camel_case_types)]
+#[derive(Debug)]
+enum IpAddrKind{
+	gg,
+	v6,
+}
+fn main() {
+	let four=IpAddrKind::gg;
+	let six=IpAddrKind::v6;
+	println!("{:?}",four);
+	println!("{:?}",six);
+	route(four);
+	route(six);
+}
+fn route(ip:IpAddrKind){
+	println!("{:?}",ip);
+}
+
+
+
