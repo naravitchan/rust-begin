67596a48b07f08c5a33a00dad24ae36ef1da510a
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-01-19 13:24:44 +0700

    [#10] - module

--- communicator/src/main.rs
+++ communicator/src/main.rs
@@ -1,4 +1,15 @@
 extern crate communicator;
+#[warn(unused_imports)]
+enum TrafficLight {
+    Red,
+    Yellow,
+    Green,
+}
+// use TrafficLight::{Red, Yellow};
+use TrafficLight::*;
 fn main() {
     communicator::client::connect();
+    let _r = Red;
+    let _y = Yellow;
+    let _g = Green;
 }
