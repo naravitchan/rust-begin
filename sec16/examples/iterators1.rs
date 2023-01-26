e3620162693a504d98dda7943e865909429e28b1
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-01-26 16:37:58 +0700

    [#16] - Closures & Iterators

---
+++ sec16/examples/iterators1.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let v1 = vec![1, 2, 3];
+    let mut v1_iter = v1.iter();
+    // println!("{:?}", v1_iter);
+    // for val in v1_iter {
+    //     println!("{:?}", val);
+    // }
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+}
