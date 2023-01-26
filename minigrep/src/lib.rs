8ee7708f739ff0934b804bd19d69502df3150fd6
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-01-26 17:48:59 +0700

    [#15] - refactor -  Closures & Iterators

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -8,12 +8,17 @@ pub struct Config {
     pub case_sensitive: bool,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not Enough Arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
         let case_sensitive = env::var("Case_Insensitive").is_err();
         Ok(Config {
             query,
@@ -68,14 +73,10 @@ Trust Me";
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,8 +5,8 @@ use std::process;
 
 // main4
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    // let args: Vec<String> = env::args().collect();
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem in Parsing {} ", err);
         process::exit(1);
     });
