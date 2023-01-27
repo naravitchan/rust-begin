c02fa1245a82935034a891ed970b5fcf077fc879
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-01-27 09:54:15 +0700

    [#17] - Cargo & Crates.io

--- sec17/sample/src/lib.rs
+++ sec17/sample/src/lib.rs
@@ -1,3 +1,11 @@
+//! Add One
+//!
+//!
+//! This crate is collection of utilities to perform
+//! calculations
+//!
+///
+///
 /// Adds to the number given
 ///
 /// #Example
@@ -9,6 +17,7 @@
 /// ```
 ///
 
+// cargo doc & cargo test
 pub fn add_one(x: i32) -> i32 {
     x + 1
 }
