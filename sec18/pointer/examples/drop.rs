3c5830967f3f33918a61717614e55205f3247eb9
Naravich Chantakanakakorn <naravit_out@hotmail.com>
2023-01-28 16:41:41 +0700

    rust

---
+++ sec18/pointer/examples/drop.rs
@@ -0,0 +1,22 @@
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data {}", self.data);
+    }
+}
+
+// cargo run --example drop
+fn main() {
+    println!("Hello, world! Drop");
+
+    let c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+    let d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+    println!("Custom created");
+}
